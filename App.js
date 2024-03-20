ed470f618d137886ae32eee3fe05dc3866ffad63
Derek <406329124@qq.com>
2024-03-20 20:43:04 +1000

    Upload Assignment
    Upload Assignment

---
+++ App.js
@@ -0,0 +1,75 @@
+import { Text, SafeAreaView, StyleSheet, View } from 'react-native';
+import React from 'react';
+
+import Task from './components/Task';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Today's Tasks */}
+      <View style={styles.taskWrapper}>
+        <View style={styles.divider} ></View>
+        <Text style={styles.sectionTitle}>Todays' Tasks</Text>
+
+        <View style={styles.items}>
+          <Task text={'task one'}></Task>
+          <Task text={'task two'}></Task>
+          <Task text={'task three'}></Task>
+        </View>
+
+      </View>
+      <View style={styles.fakeBox}>
+        <Text style={styles.fakeText}>ADD NEW TODO</Text>
+      </View>
+
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  taskWrapper:{
+    paddingTop:5,
+    paddingHorizontal: 20,
+  },
+  sectionTitle:{
+    marginTop: 100,
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items:{
+    marginTop: 30,
+  },
+  divider:{
+    backgroundColor: 'blue',
+    height:1,
+    alignself: "center",
+    flex:1,
+  },
+  fakeBox:{
+    marginTop: 'auto',
+    padding:15,
+    backgroundColor: 'blue',
+    borderRadius:10,
+  },
+  fakeText:{
+    textAlign: 'center',
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
