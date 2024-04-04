5bd416576cd36ab2454e7ddc5c58ab551fc1813f
Derek <101934458+dderekk@users.noreply.github.com>
2024-04-04 17:20:57 +1000

    Milestone 2-Enhancing Functionality and Navigation
    Adding and Enhancing Functionality and Navigation

--- App.js
+++ App.js
@@ -1,77 +1,18 @@
-import { Text, SafeAreaView, StyleSheet, View, Button } from 'react-native';
-import React from 'react';
+import { Home } from "./screen/Home";
+import { NewToDo } from "./screen/NewToDo";
 
-import { COLORS } from './components/Color';
-import Task from './components/Task';
+// This is for navigation part
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-
-export default function App() {
+const Stack = createStackNavigator();
+export default function App(){
   return (
-    <View style={styles.container}>
-      {/* Today's Tasks */}
-      <View style={styles.taskWrapper}>
-        <View style={styles.divider} ></View>
-        <Text style={styles.sectionTitle}>My Todo List</Text>
-
-        <View style={styles.items}>
-          <Task text={'task one'}></Task>
-          <Task text={'task two'}></Task>
-          <Task text={'task three'}></Task>
-        </View>
-
-      </View>
-      <View style={styles.fakeBox}>
-        <Button title= "ADD NEW TODO" />
-      </View>
-      
-
-
-
-    </View>
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component = {Home} />
+      <Stack.Screen name="NewToDo" component={NewToDo} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    backgroundColor: COLORS.BBC,
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  taskWrapper:{
-    paddingTop:5,
-    paddingHorizontal: 20,
-  },
-  sectionTitle:{
-    marginTop: 100,
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  items:{
-    marginTop: 30,
-  },
-  divider:{
-    backgroundColor: 'blue',
-    height:1,
-    alignself: "center",
-    flex:1,
-  },
-  fakeBox:{
-    marginTop: 200,
-    padding:6,
-    backgroundColor: COLORS.ButtonOutter,
-    borderRadius:10,
-  },
-  fakeText:{
-    textAlign: 'center',
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-});
